ef0cdafdf303ba22325a4e9f3641147a24849fe3
zvezdin <git@zvezd.in>
2020-02-16 16:06:01 +0000

    Initial commit with dummy webpage

---
+++ component---src-pages-about-js-70dc852e056f12ab2195.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"Hey"}),r.a.createElement("p",null,"Suchwow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-70dc852e056f12ab2195.js.map
